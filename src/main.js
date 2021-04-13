7b6e2fad7537a0452f9f22cc1f5628e4d04d8308
user-zhl <zhangxiaohu3@163.com>
2021-04-13 15:26:39 +0800

    zhl-first

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import Vant from "vant";
 import "vant/lib/index.css";
 import App from "./App.vue";
 import router from "./router";
+import Element from "element-ui";
+import less from 'less';
+
 
 Vue.use(Vant);
+Vue.use(less);
+Vue.use(Element);
 Vue.config.productionTip = false;
 
 new Vue({
