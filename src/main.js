d8c28c0a07782d1881bb4b0fb24d424e99850863
zhanming <3125390848@qq.com>
2021-04-13 17:22:48 +0800

    第一次合并

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 import Vue from "vue";
-import ElementUI from 'element-ui';
+
 import 'element-ui/lib/theme-chalk/index.css';
 import Vant from "vant";
 import "vant/lib/index.css";
 import App from "./App.vue";
 import router from "./router";
 import VueCookies from 'vue-cookies'
-Vue.use(VueCookies)
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-
-import less from "less";
-
+Vue.use(VueCookies)
 Vue.use(ElementUI);
 Vue.use(Vant);
-Vue.use(less);
-Vue.use(Element);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   render: (h) => h(App),
