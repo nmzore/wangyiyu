1993c4a7372e69e8b0807ae42cca1483cadfc3a5
dkx <2395548086@qq.com>
2021-04-14 08:36:20 +0800

    dkx-222

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -67,12 +67,10 @@
         <h3>私人订制</h3>
         <button>更多 &gt;</button>
       </div>
-
       <div class="sr-shua">
         <van-icon name="replay" :size="3" />
         <span>好听的华语歌曲精选</span>
       </div>
-
       <div class="sr-bot">
         <div class="sr-bot-01" v-for="item in srs" :key="item.id">
           <div v-for="aa in item" :key="aa.id">
@@ -96,7 +94,12 @@
       </div>
       <el-carousel :interval="4000" type="card" height="200px">
         <el-carousel-item v-for="item in 3" :key="item">
-          <h3 class="medium">{{ item }}</h3>
+          <h3 class="medium">
+            <img
+              src="https://img0.baidu.com/it/u=1017673628,2213309026&fm=26&fmt=auto&gp=0.jpg"
+              alt=""
+            />
+          </h3>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -109,12 +112,94 @@
       </div>
       <el-row>
         <el-col :span="12">
-          <div class="grid-content bg-purple">范丞丞&程潇的专辑：遥望</div>
+          <div class="grid-content bg-purple">
+            <span>今天</span>
+            <p>范丞丞&程潇的专辑：遥望</p>
+          </div>
         </el-col>
         <el-col :span="12">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">
+            <img
+              src="https://img2.baidu.com/it/u=288820895,2189925987&fm=15&fmt=auto&gp=0.jpg"
+              alt=""
+            />
+          </div>
         </el-col>
       </el-row>
+      <el-row>
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
+            <span>01-01</span>
+            <p>预告-王贰浪的最新作品：悔过</p>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple-light">
+            <img
+              src="https://img2.baidu.com/it/u=787390020,1021945622&fm=15&fmt=auto&gp=0.jpg"
+              alt=""
+            />
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <!-- 专属场景歌单 -->
+    <div class="tj">
+      <div class="tj-top">
+        <h3>专属场景歌单</h3>
+        <button>更多 &gt;</button>
+      </div>
+      <div class="tj-bot">
+        <van-grid
+          :border="false"
+          :column-num="3"
+          :gutter="5"
+          :icon-size="100"
+          v-for="item in gds"
+          :key="item.id"
+        >
+          <van-grid-item>
+            <van-image :src="item.picUrl" />
+            <p>{{ item.name }}</p>
+          </van-grid-item>
+        </van-grid>
+      </div>
+    </div>
+
+    <!-- LOOK直播 -->
+    <div class="tj zb">
+      <div class="tj-top">
+        <h3>LOOK直播</h3>
+        <button>更多 &gt;</button>
+      </div>
+      <div class="tj-bot">
+        <van-grid
+          :border="false"
+          :column-num="3"
+          :gutter="5"
+          :icon-size="100"
+          v-for="item in gds"
+          :key="item.id"
+        >
+          <van-grid-item>
+            <van-image :src="item.picUrl" />
+            <p>{{ item.name }}</p>
+          </van-grid-item>
+        </van-grid>
+      </div>
+    </div>
+
+    <!-- 新歌 新碟 数字专辑 -->
+    <div class="sr">
+      <div class="tj-top">
+        <van-tabs v-model="active">
+          <van-tab title="标签 1">1</van-tab>
+          <van-tab title="标签 2">2</van-tab>
+          <van-tab title="标签 3">3</van-tab>
+        </van-tabs>
+        <button>更多 &gt;</button>
+      </div>
     </div>
 
     <p>1</p>
@@ -137,6 +222,21 @@ export default {
       banners: [],
       gds: [],
       srs: [],
+      imags: [
+        {
+          img:
+            "https://img2.baidu.com/it/u=2732402456,4109538454&fm=26&fmt=auto&gp=0.jpg",
+        },
+        {
+          img:
+            "https://img1.baidu.com/it/u=2966533095,4259557322&fm=26&fmt=auto&gp=0.jpg",
+        },
+        {
+          img:
+            "https://img0.baidu.com/it/u=1017673628,2213309026&fm=26&fmt=auto&gp=0.jpg",
+        },
+      ],
+      active: 0,
     };
   },
   components: {},
@@ -144,6 +244,17 @@ export default {
   method: {
     //私人订制 刷新
     // reset() {},
+    //私人订制 请求接口
+    // srList() {
+    //   axios.get("http://localhost:3000/album/newest").then((res) => {
+    //     var result = [];
+    //     for (var i = 0; i < res.data.albums.length; i += 3) {
+    //       result.push(res.data.albums.slice(i, i + 3));
+    //     }
+    //     // console.log(result);
+    //     this.srs = result;
+    //   });
+    // },
   },
   created() {
     //轮播图请求
@@ -158,7 +269,8 @@ export default {
         // console.log(res);
         this.gds = res.data.result;
       });
-    //私人订制
+    //私人订制 调用接口
+    // this.srList();
     axios.get("http://localhost:3000/album/newest").then((res) => {
       var result = [];
       for (var i = 0; i < res.data.albums.length; i += 3) {
@@ -267,12 +379,31 @@ export default {
   display: flex;
   overflow-x: auto;
   flex-wrap: nowrap;
+  margin: 0.8rem 0;
+}
+.sr-bot-01 {
+  margin-right: 1.5rem;
 }
 .van-card {
   width: 300px;
+  background: #fff;
+}
+.van-card__content {
+  border-bottom: solid 1px rgb(231, 229, 229);
+}
+.van-card__content p {
+  font-size: 16px;
+  line-height: 20px;
+  margin: 0.4rem 0;
+}
+.van-card__content span {
+  font-size: 12px;
 }
 
 /* 精选音乐视频 */
+.el-carousel--card {
+  z-index: -1000;
+}
 .el-carousel__item {
   width: 150px;
   margin: 2rem 0 0 1rem;
@@ -298,29 +429,43 @@ export default {
   margin-top: 1.5rem;
 }
 .el-row {
-  margin-bottom: 20px;
-  /* &:last-child {
-    margin-bottom: 0;
-  } */
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
+  border-top: solid 3px rgb(240, 239, 239);
+  border-bottom: solid 1px rgb(240, 239, 239);
 }
 .bg-purple {
-  background: #d3dce6;
+  height: 6.2rem;
+  line-height: 3rem;
+  font-size: 12px;
 }
-.bg-purple-light {
-  background: #e5e9f2;
+.bg-purple span {
+  margin-left: 1rem;
 }
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+.bg-purple p {
+  margin-left: 1rem;
 }
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
+.bg-purple-light img {
+  width: 80px;
+  float: right;
+  margin: 1rem;
 }
+
+/* 专属场景歌单 */
+
+/* LOOK直播 */
+.zb {
+  border-top: solid 5px rgb(240, 239, 239);
+  border-bottom: solid 5px rgb(240, 239, 239);
+}
+
+/* 新歌 */
+/* .van-tabs__wrap{
+  width: 100px;
+  background: burlywood;
+} */
+/* .van-tabs__content {
+  display: flex;
+  overflow-x: auto;
+  flex-wrap: nowrap;
+  margin: 0.8rem 0;
+} */
 </style>
